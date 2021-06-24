2fdbdfa2aa1adda53d8a67ddaeb0a8a27fe4acb4
takumi_kurogi <takumines1@gmail.com>
2021-06-24 18:28:21 +0900

    httprouterの使用

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_practice
 
 go 1.15
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	golang.org/x/tools v0.1.3
+)
--- main.go
+++ main.go
@@ -1,44 +1,21 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
-	"net"
-	"os"
-	"time"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "hello , %s!\n", p.ByName("name"))
+}
+
 func main() {
-	max := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, _ := rand.Int(rand.Reader, max)
-	subject := pkix.Name{
-		Organization:       []string{"Manning Publications Co."},
-		OrganizationalUnit: []string{"Books"},
-		CommonName:         "Go Web Programming",
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject:      subject,
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
-		KeyUsage:     x509.KeyUsageDataEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
-	}
-
-	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
-	certOut, _ := os.Create("cert.pem")
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-
-	keyOut, _ := os.Create("Key.pem")
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
-	keyOut.Close()
+	server.ListenAndServe()
 }
