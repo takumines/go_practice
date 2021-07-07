46bb833b401d8838399c269f0716726791b40e45
takumi_kurogi <takumines1@gmail.com>
2021-07-07 16:42:58 +0900

    起動時にmigrateが走るように設定

--- sample-api/db/connect.go
+++ sample-api/db/connect.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/takumines/go_practice/sample-api/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,9 +11,14 @@ var DB *gorm.DB
 func Init() {
 	dsn := "host=127.0.0.1 user=test password=test1234 dbname=test port=5432 sslmode=disable TimeZone=Asia/Tokyo"
 	connect, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 
 	DB = connect
+
+	err = connect.AutoMigrate(&models.Post{}, &models.Comment{})
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
 }
