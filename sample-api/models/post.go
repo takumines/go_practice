0c576679b788fc44648baefbaee3c3f93c8da749
takumi_kurogi <takumines1@gmail.com>
2021-07-13 19:08:03 +0900

    記事が存在しているかチェックするメソッドを作った

--- sample-api/models/post.go
+++ sample-api/models/post.go
@@ -5,14 +5,12 @@ import (
 )
 
 type Post struct {
-	// gorm.Modelはあまり使わない
-	gorm.Model
+	ID       uint      `json:"id" gorm:"primarykey"`
 	Content  string    `json:"content"`
 	Author   string    `json:"author"`
 	Comments []Comment `json:"comments" gorm:"foreignKey:PostID"`
 }
 
-// AllPost or FindAllPost
 func AllPost(db *gorm.DB) (posts []Post, err error) {
 	rows, err := db.Find(&posts).Rows()
 	if err != nil {
@@ -22,9 +20,14 @@ func AllPost(db *gorm.DB) (posts []Post, err error) {
 	return
 }
 
-// GetPost or FindPostOfId
 func GetPost(id int, db *gorm.DB) (post Post, err error) {
 	post = Post{}
 	err = db.Preload("Comments").First(&post, id).Error
+	return
+}
+
+func CheckPost(id int, db *gorm.DB) (err error) {
+	post := Post{}
+	err = db.Where("id = ?", id).First(&post).Error
 	return
 }
